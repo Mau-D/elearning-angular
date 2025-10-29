9d1e7e52490b42955419c206c17c82320b702102
Mau-D <harveymaud@gmail.com>
2025-10-29 09:05:52 -0400

    fix installation w Nx

--- quest-04/angular-formations/apps/video-game-app/src/app/app.spec.ts
+++ quest-04/angular-formations/apps/video-game-app/src/app/app.spec.ts
@@ -2,6 +2,7 @@ import { TestBed } from '@angular/core/testing';
 import { App } from './app';
 import { NxWelcome } from './nx-welcome';
 
+
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
--- quest-04/angular-formations/apps/video-game-app/src/app/app.ts
+++ quest-04/angular-formations/apps/video-game-app/src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import { ListVideoGames } from '../features/video-games/components/list-video-games/list-video-games';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  imports: [ListVideoGames],
+  imports: [ListVideoGames, NgbAlertModule],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
