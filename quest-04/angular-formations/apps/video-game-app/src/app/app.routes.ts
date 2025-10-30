d0fa0de721aef9ffea85d3696235de1a40c0b963
Mau-D <harveymaud@gmail.com>
2025-10-30 09:36:18 -0400

    remove redirect

--- quest-04/angular-formations/apps/video-game-app/src/app/app.routes.ts
+++ quest-04/angular-formations/apps/video-game-app/src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Route } from '@angular/router';
 import { PageNotFound } from '../shared/page-not-found/page-not-found';
 
 export const appRoutes: Route[] = [
-  {
-    path: '',
-    redirectTo:'/video-games',
-    pathMatch: 'full',
-    // children: videoGamesRoutes,
-  },
+  // {
+  //   path: '',
+  //   redirectTo:'video-games',
+  //   pathMatch: 'full',
+  //   // children: videoGamesRoutes,
+  // },
   {
     path: 'video-games',
     loadChildren: ()=>import('../features/video-games/video-games.routes').then(item => item.videoGamesRoutes)
