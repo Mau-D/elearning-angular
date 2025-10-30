0bd48d8450882b99fa57618f9364179a06996ea7
Mau-D <harveymaud@gmail.com>
2025-10-29 21:42:35 -0400

    params management w signal and computed + snapshot

--- quest-04/angular-formations/apps/video-game-app/src/features/video-games/components/edit-video-game/edit-video-game.ts
+++ quest-04/angular-formations/apps/video-game-app/src/features/video-games/components/edit-video-game/edit-video-game.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, computed, inject, OnInit } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
 
@@ -10,8 +10,26 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class EditVideoGame implements OnInit {
   private readonly route = inject(ActivatedRoute);
-videoGameParamsS = toSignal(this.route.params)
+videoGameParamsS = toSignal(this.route.params);
+// Le computed permet à videoGameId de rester synchronisé avec le paramètre id de l'URL, 
+// sans que vous ayez à écrire de logique de souscription (subscribe) ou de nettoyage 
+// (unsubscribe). Il est performant car il ne recalcule la valeur que si ses dépendances 
+// changent.
+videoGameId = computed(() => {
+  const params = this.videoGameParamsS();
+  return params?.['id'] ?? '0'}
+);
+// videoGameId = computed(() => {
+//   let id =0
+//   const params = this.videoGameParamsS()
+//   if(params){
+//     id = params[id];
+//     return id;
+//   }
+//   return;
+// })
   ngOnInit(): void {
+    console.info('snapshot',this.route.snapshot.params['id']);
     this.route.params.subscribe({
       next: (item) => {
         console.info(item['id']);
