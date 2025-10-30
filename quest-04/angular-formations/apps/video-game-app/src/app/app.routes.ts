21d81a607fdfffc011cef0aa2afce6beea7846f4
Mau-D <harveymaud@gmail.com>
2025-10-30 09:49:47 -0400

    Remove type Array for Routes, ts recognizes

--- quest-04/angular-formations/apps/video-game-app/src/app/app.routes.ts
+++ quest-04/angular-formations/apps/video-game-app/src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Route } from '@angular/router';
 import { PageNotFound } from '../shared/page-not-found/page-not-found';
 
-export const appRoutes: Route[] = [
+// export const appRoutes: Route[] = [ ===> ===>ts reconnait le type Array
+export const appRoutes = [
   // {
   //   path: '',
   //   redirectTo:'video-games',
@@ -10,7 +11,10 @@ export const appRoutes: Route[] = [
   // },
   {
     path: 'video-games',
-    loadChildren: ()=>import('../features/video-games/video-games.routes').then(item => item.videoGamesRoutes)
+    loadChildren: () =>
+      import('../features/video-games/video-games.routes').then(
+        (item) => item.videoGamesRoutes
+      ),
   },
   // ...videoGamesRoutes,
   {
--- quest-04/angular-formations/apps/video-game-app/src/features/video-games/video-games.routes.ts
+++ quest-04/angular-formations/apps/video-game-app/src/features/video-games/video-games.routes.ts
@@ -2,7 +2,8 @@ import { Route } from '@angular/router';
 import { ListVideoGames } from './components/list-video-games/list-video-games';
 import { EditVideoGame } from './components/edit-video-game/edit-video-game';
 
-export const videoGamesRoutes: Route[] = [
+// export const videoGamesRoutes: Route[] = [ ===>ts reconnait le type Array
+export const videoGamesRoutes = [
   // '/video-game/'
   {
     path: '',
